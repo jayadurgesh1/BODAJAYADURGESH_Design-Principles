4ea9d74cdccdffede2b7b5642e5dd3830867da93
jayadurgesh1 <jayadurgesh1@gmail.com>
2020-02-09 16:08:10 +0530

    committing calculator

---
+++ src/main/java/com/epam_task3/App.java
@@ -0,0 +1,35 @@
+package com.epam_task3;
+import java.util.Scanner;
+class App extends Operations
+{
+	void App()
+	{
+		Scanner s=new Scanner (System.in);
+		System.out.println("\n\nEnter the 1st number for Math operation");
+		float num1= s.nextFloat();
+		System.out.println("Enter the 2nd number for Math operation");
+		float num2= s.nextFloat();
+		System.out.println("SIMPLE CALCULATOR\nSELECT THE OPERATION\n1.ADD\n2.SUBRACT\n3.MULTIPLICATION\n4.DIVISION\n5.EXIT");
+		int operation=s.nextInt();
+		switch(operation)
+		{
+			case 1: System.out.println("After Adding "+num1+" and "+num2+" is "+add(num1,num2));
+			break;
+			case 2: System.out.println("After Subracting "+num1+" and "+num2+" is "+subract(num1,num2));
+			break;
+			case 3: System.out.println("After  Multiplying"+num1+" and "+num2+" is "+multiply(num1,num2));
+			break;
+			case 4: System.out.println("After Dividing "+num1+" and "+num2+" is "+divide(num1,num2));
+			break;
+			case 5: break;
+			default:
+			System.out.println("ENTER VALID OPTION");
+			App();
+		}
+	}
+	public static void main(String args[])
+	{
+		App obj1=new App();
+		obj1.App();
+	}
+}
---
+++ src/main/java/com/epam_task3/Operations.java
@@ -0,0 +1,20 @@
+package com.epam_task3;
+class Operations
+{
+	float add(float a,float b)
+	{
+		return a+b;
+	}
+	float subract(float a,float b)
+	{
+		return a-b;
+	}
+	float multiply(float a,float b)
+	{
+		return a*b;
+	}
+	float divide(float a,float b)
+	{
+		return a/b;
+	}
+}
